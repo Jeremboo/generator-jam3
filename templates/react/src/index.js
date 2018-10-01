76c5f38a7bac28c60407f5e63f2eef5b98c27389
Jérémie Boulay <jeremi.boulay@gmail.com>
2018-10-01 15:45:33 -0400

    :arrow_up: update babel to Babel 7 for react template

--- templates/react/src/index.js
+++ templates/react/src/index.js
@@ -1,5 +1,5 @@
 'use strict';
-import 'babel-polyfill';
+import '@babel/polyfill';
 import framework from './framework';
 import domready from 'domready';
 if (process.env.NODE_ENV === 'development') require('./util/stats')();
